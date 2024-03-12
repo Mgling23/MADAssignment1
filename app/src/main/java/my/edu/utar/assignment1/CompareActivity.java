6b8f6b32d28d12aabe5dbea19be96d33430fe60c
Mgling23 <krikland0522@gmail.com>
2024-03-12 22:14:42 +0800

    Update 1

--- app/src/main/java/my/edu/utar/assignment1/CompareActivity.java
+++ app/src/main/java/my/edu/utar/assignment1/CompareActivity.java
@@ -1,10 +1,15 @@
 package my.edu.utar.assignment1;
 
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +19,8 @@ public class CompareActivity extends AppCompatActivity {
 
     private TextView compareText;
     private Button compNumOne, compNumTwo, compSubmitBtn;
-    private int number1 , number2;
+    private int number1 , number2, count;
+    private boolean num1 = false, num2 = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,25 +32,40 @@ public class CompareActivity extends AppCompatActivity {
         compNumTwo = findViewById(R.id.compNumTwo);
         compSubmitBtn = findViewById(R.id.compSubmitBtn);
 
+
+
         generateNumbers();
 
         compNumOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkAnswer(number1, number2);
+                num1 = true;
+                num2 = false;
+                compNumOne.setBackgroundColor(getResources().getColor(R.color.btn_selected));
+                compNumTwo.setBackgroundColor(getResources().getColor(R.color.btn_origin));                //checkAnswer(number1, number2);
             }
         });
 
         compNumTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkAnswer(number2, number1);
+                num2 = true;
+                num1 = false;
+                compNumTwo.setBackgroundColor(getResources().getColor(R.color.btn_selected));
+                compNumOne.setBackgroundColor(getResources().getColor(R.color.btn_origin));
+                //checkAnswer(number2, number1);
             }
         });
 
         compSubmitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (num1){
+                    checkAnswer(number1,number2);
+                }
+                else
+                    checkAnswer(number2,number1);
+
                 generateNumbers();
             }
         });
@@ -60,9 +81,11 @@ public class CompareActivity extends AppCompatActivity {
             number2 = random.nextInt(10) + 1;
         }
 
+
         compNumOne.setText(String.valueOf(number1));
         compNumTwo.setText(String.valueOf(number2));
-
+        compNumOne.setBackgroundColor(getResources().getColor(R.color.btn_origin));
+        compNumTwo.setBackgroundColor(getResources().getColor(R.color.btn_origin));
         compareText.setText("Please choose the bigger number");
     }
 
